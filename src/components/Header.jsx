141a8c189c2f66edbf438fee007ad9dadf90b2e4
MFMcNolt <mfergusonmcnulty@gmail.com>
2024-01-22 14:55:20 +1100

    updated files to delete main

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navigation from './Navigation';
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <Link to="/" className="header-brand">
+        <h1>Marion</h1>
+      </Link>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
