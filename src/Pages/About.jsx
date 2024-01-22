e059cfc6e76a9acb5c3ab4415aa577bd96980918
MFMcNolt <mfergusonmcnulty@gmail.com>
2024-01-22 15:58:27 +1100

    added in image

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,15 +1,20 @@
+import React from 'react';
+import yourImage from '../assets/meimage.png';
+
 const About = () => {
-    return (
-      <div className="about-container">
-        <div className="about-header">
-          <h2>About Marion</h2>
-        </div>
-        <div className="about-content">
-          <p>Hello! I am a new muma bear who is completely bestotted with being one. With a toe always in the tech world, I wanted to be able to create me own websites and applications that can serve to help parents and how they navigate the wonderful and messy world of BEING A PARENT!
-  </p>
-        </div>
+  return (
+    <div className="about-container">
+      <div className="about-header">
+        <h2>About Marion</h2>
       </div>
-    );
-  };
-  
-  export default About;
+      <div className="about-content">
+        <img src={yourImage} alt="profile pic" />
+        <p>
+          Hello! I am a new mama bear who is completely besotted with being one. With a toe always in the tech world, I wanted to be able to create my own websites and applications that can serve to help parents and how they navigate the wonderful and messy world of BEING A PARENT!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
