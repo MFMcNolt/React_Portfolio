91c1ca87b95db2153ba62530ca61d31db8873010
MFMcNolt <mfergusonmcnulty@gmail.com>
2024-01-22 16:03:45 +1100

    photo

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -8,7 +8,11 @@ const About = () => {
         <h2>About Marion</h2>
       </div>
       <div className="about-content">
-        <img src={yourImage} alt="profile pic" />
+        <img
+          src={yourImage}
+          alt="profile pic"
+          style={{ width: '200px', height: 'auto' }} 
+        />
         <p>
           Hello! I am a new mama bear who is completely besotted with being one. With a toe always in the tech world, I wanted to be able to create my own websites and applications that can serve to help parents and how they navigate the wonderful and messy world of BEING A PARENT!
         </p>
